c4ae44aa2a04521a426305fa6bbc54e69d73dc92
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2019-06-18 10:17:38 -0300

    Add whitenoise to serve staticfiles
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- src/call_api/settings.py
+++ src/call_api/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'call_api.urls'
@@ -164,6 +165,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'public')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
